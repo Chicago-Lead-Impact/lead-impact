d9b5cb6836dad1f7055cb29c4d07c3e18695ed98
YasiTL <yasaman.most@gmail.com>
2023-04-17 16:10:46 -0500

    Fonts updated (thanks bestie)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from "react";
 
+import { Raleway } from 'next/font/google';
+const raleway = Raleway({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
+    <div className={raleway.className}>
       <Component {...pageProps} />
+    </div>
     )
 }
